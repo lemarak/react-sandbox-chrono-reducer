ed90615eb91dd6d63651cdf745b58f4a5bfba69d
Stéphane Dauguet <sdauguet@gmail.com>
2022-11-10 10:30:26 +0100

    run chrono

--- src/Chrono/Chrono.js
+++ src/Chrono/Chrono.js
@@ -1,17 +1,56 @@
-import { useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import "./Chrono.css";
 import PauseImg from "../Images/pause.svg";
 import PlayImg from "../Images/play.svg";
 import ResetImg from "../Images/reset.svg";
 
 const Chrono = () => {
-  const [sessionTime, setSessionTime] = useState(1500);
-  const [sessionTimeFixed, setSessionTimeFixed] = useState(1500);
+  const [sessionTime, setSessionTime] = useState(60);
+  const [sessionTimeFixed, setSessionTimeFixed] = useState(60);
 
-  const [sessionBreak, setSessionBreak] = useState(300);
-  const [sessionBreakFixed, setSessionBreakFixed] = useState(300);
+  const [breakTime, setBreakTime] = useState(60);
+  const [breakTimeFixed, setBreakTimeFixed] = useState(60);
 
   const [workingChrono, setWorkingChrono] = useState(false);
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "TICK":
+        if (sessionTime >= 0) {
+          setSessionTime(sessionTime - 1);
+        } else if (breakTime >= 1) {
+          setBreakTime(breakTime - 1);
+        } else if (sessionTime <= 0 && breakTime <= 0) {
+          setSessionTime(sessionTimeFixed);
+          setBreakTime(breakTimeFixed);
+        }
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer);
+
+  useEffect(() => {
+    let id;
+    if (workingChrono) {
+      id = window.setInterval(() => {
+        dispatch({ type: "TICK" });
+      }, 100);
+    }
+
+    return () => {
+      window.clearInterval(id);
+    };
+  }, [workingChrono]);
+
+  const togglePause = () => {
+    setWorkingChrono(!workingChrono);
+  };
+
+  const resetChrono = () => {
+    setWorkingChrono(false);
+    setSessionTime(sessionTimeFixed);
+    setBreakTime(breakTimeFixed);
+  };
 
   return (
     <div className="container-chrono">
@@ -23,18 +62,26 @@ const Chrono = () => {
         </div>
         <div className="box-btns break">
           <button className="minus">-</button>
-          <span>{sessionBreakFixed / 60}</span>
+          <span>{breakTimeFixed / 60}</span>
           <button className="plus">+</button>
         </div>
       </div>
       <h1>
-        <span>CHRONO</span>
+        {sessionTime >= 0 ? (
+          <span>{`${Math.trunc(sessionTime / 60)} : ${
+            sessionTime % 60 < 10 ? "0" + (sessionTime % 60) : sessionTime % 60
+          }`}</span>
+        ) : (
+          <span>{`${Math.trunc(breakTime / 60)} : ${
+            breakTime % 60 < 10 ? "0" + (breakTime % 60) : breakTime % 60
+          }`}</span>
+        )}
       </h1>
       <div className="container-controllers">
-        <button>
+        <button onClick={togglePause}>
           <img src={workingChrono ? PauseImg : PlayImg} alt="play" />
         </button>
-        <button>
+        <button onClick={resetChrono}>
           <img src={ResetImg} alt="reset" />
         </button>
       </div>
