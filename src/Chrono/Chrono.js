0367f63ec608d1be89d1df216df32b58965f6796
Stéphane Dauguet <sdauguet@gmail.com>
2022-11-10 09:34:14 +0100

    Design Chrono.js

--- src/Chrono/Chrono.js
+++ src/Chrono/Chrono.js
@@ -1,9 +1,43 @@
+import { useState } from "react";
 import "./Chrono.css";
+import PauseImg from "../Images/pause.svg";
+import PlayImg from "../Images/play.svg";
+import ResetImg from "../Images/reset.svg";
 
 const Chrono = () => {
+  const [sessionTime, setSessionTime] = useState(1500);
+  const [sessionTimeFixed, setSessionTimeFixed] = useState(1500);
+
+  const [sessionBreak, setSessionBreak] = useState(300);
+  const [sessionBreakFixed, setSessionBreakFixed] = useState(300);
+
+  const [workingChrono, setWorkingChrono] = useState(false);
+
   return (
-    <div>
-      <h1>Chrono</h1>
+    <div className="container-chrono">
+      <div className="container-config">
+        <div className="box-btns session">
+          <button className="minus">-</button>
+          <span>{sessionTimeFixed / 60}</span>
+          <button className="plus">+</button>
+        </div>
+        <div className="box-btns break">
+          <button className="minus">-</button>
+          <span>{sessionBreakFixed / 60}</span>
+          <button className="plus">+</button>
+        </div>
+      </div>
+      <h1>
+        <span>CHRONO</span>
+      </h1>
+      <div className="container-controllers">
+        <button>
+          <img src={workingChrono ? PauseImg : PlayImg} alt="play" />
+        </button>
+        <button>
+          <img src={ResetImg} alt="reset" />
+        </button>
+      </div>
     </div>
   );
 };
